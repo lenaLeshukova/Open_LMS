f0384246e56daac2f9b2d6fc5f9d89843be177a3
Lena Leshukova <lena000183@gmail.com>
2026-08-24 23:25:52 +0300

    дз 30.2 финальный

--- config/urls.py
+++ config/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
     # урлы LMS
     path('api/', include('lms.urls', namespace='lms')),
     path('api/', include('users.urls', namespace='users')),
